a056a1c2b55501cfa07a188899cae1adcc2fa185
Maks Khomiak <makskhomiak67@gmail.com>
2025-09-03 22:18:24 +0300

    Replace ResourcesDatabase with Addressables system integration for resource management.

--- Assets/_Game/_Scripts/Loaders/ResourcesDataLoader.cs
+++ Assets/_Game/_Scripts/Loaders/ResourcesDataLoader.cs
@@ -6,7 +6,7 @@ namespace _Game._Scripts.Loaders {
  
   public class ResourcesDataLoader : ILoader
   {
-    private const string ADDRESS_KEY = "ResourcesData";
+    private const string ADDRESS_KEY = "Assets/_Game/SO/Resources_moved/Resources_movedDatabase.asset";
 
     public async UniTask LoadAsync()
     {
