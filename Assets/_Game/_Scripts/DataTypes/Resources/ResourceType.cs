cb51deaad9704bc2e6742bcb1583e25b33008c33
Maks Khomiak <makskhomiak67@gmail.com>
2025-09-06 18:08:29 +0300

    Гpdate `ResourceType` enum and SO assets to separate raw and processed resources, and add new resource sprites.

--- Assets/_Game/_Scripts/DataTypes/Resources/ResourceType.cs
+++ Assets/_Game/_Scripts/DataTypes/Resources/ResourceType.cs
@@ -1,12 +1,17 @@
 namespace _Game._Scripts.DataTypes.Resources {
   public enum ResourceType {
     None = 0,
-    Coal = 1,
-    Copper = 2,
+    CoalIngot = 1,
+    CopperIngot = 2,
     Gold = 3,
     Diamond = 4,
     Emerald = 5,
     Wood = 6,
-    Hardwood = 7
+    Hardwood = 7,
+    
+    Plank = 100,
+    StrongPlank = 101,
+    Coal = 102,
+    Copper = 103,
   }
 }
